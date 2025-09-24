ebf03ecb9e48a7f51db38b54bccf92021b3346b5
Meb Bjk <32273221+mebbjk@users.noreply.github.com>
2025-09-25 02:03:57 +0300

    Update vite.config.ts

--- emotional-truth-_-et/vite.config.ts
+++ emotional-truth-_-et/vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Expose environment variables to the client-side code by performing a text-replacement.
-  // This ensures the variables are available even if the runtime doesn't support import.meta.env.
-  define: {
-    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
-  }
 })
